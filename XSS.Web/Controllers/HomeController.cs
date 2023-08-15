79f985283669f1af78c361e95d7186e7cbd86d9f
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2023-08-15 16:48:34 +0300

    XSS Attacking

---
+++ XSS.Web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using XSS.Web.Models;
+
+namespace XSS.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult CommentAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CommentAdd(string name,string comment)
+        {
+            ViewBag.Name = name;
+            ViewBag.Comment = comment;
+            return View();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ XSS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XSS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
