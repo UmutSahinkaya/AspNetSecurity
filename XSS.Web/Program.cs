c4b8d24a29ee854563d0dbdd64af74563b04719a
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2023-08-16 09:47:00 +0300

    app.UseHsts() ve app.UseHttpsRedirection()

--- XSS.Web/Program.cs
+++ XSS.Web/Program.cs
@@ -15,10 +15,10 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts();//Strict-Transport-Security header(response)
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection();//eðer kullanýcý http isteði atarsa kullanýcýyý https olan (ssl) sertifikalý sayfaya yönlendir. 
 app.UseStaticFiles();
 
 app.UseRouting();
