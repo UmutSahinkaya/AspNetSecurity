914d25b893dce73fe95ea9f973716f9eac54dafc
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2023-08-16 09:02:58 +0300

    url,javascript,html Encode classes dependency injection this program

--- XSS.Web/Controllers/HomeController.cs
+++ XSS.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Encodings.Web;
 using XSS.Web.Models;
 
 namespace XSS.Web.Controllers
@@ -7,12 +8,16 @@ namespace XSS.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private HtmlEncoder _htmlEncoder;
+        private JavaScriptEncoder _javascriptEncoder;
+        private UrlEncoder _urlEncoder;
+        public HomeController(ILogger<HomeController> logger, HtmlEncoder htmlEncoder, JavaScriptEncoder javascriptEncoder, UrlEncoder urlEncoder)
         {
             _logger = logger;
+            _htmlEncoder = htmlEncoder;
+            _javascriptEncoder = javascriptEncoder;
+            _urlEncoder = urlEncoder;
         }
-
         public IActionResult CommentAdd()
         {
             HttpContext.Response.Cookies.Append("email", "ahmetsahin@gmail.com");
@@ -20,21 +25,21 @@ namespace XSS.Web.Controllers
 
             if (System.IO.File.Exists("comment.txt"))
             {
-                ViewBag.comments=System.IO.File.ReadAllLines("comment.txt");
+                ViewBag.comments = System.IO.File.ReadAllLines("comment.txt");
             }
 
             return View();
         }
         [HttpPost]
-        public IActionResult CommentAdd(string name,string comment)
+        public IActionResult CommentAdd(string name, string comment)
         {
+            string encodeName = _urlEncoder.Encode(name);
             ViewBag.Name = name;
             ViewBag.Comment = comment;
 
-            System.IO.File.AppendAllText("comment.txt",$"{name}-{comment}\n");
+            System.IO.File.AppendAllText("comment.txt", $"{name}-{comment}\n");
             return Redirect("CommentAdd");
         }
-
 
         public IActionResult Index()
         {
@@ -51,5 +56,28 @@ namespace XSS.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        #region CommentADD
+        //public IActionResult CommentAdd()
+        //{
+        //    HttpContext.Response.Cookies.Append("email", "ahmetsahin@gmail.com");
+        //    HttpContext.Response.Cookies.Append("password", "asdasd1A.");
+
+        //    if (System.IO.File.Exists("comment.txt"))
+        //    {
+        //        ViewBag.comments = System.IO.File.ReadAllLines("comment.txt");
+        //    }
+
+        //    return View();
+        //}
+        //[HttpPost]
+        //public IActionResult CommentAdd(string name, string comment)
+        //{
+        //    ViewBag.Name = name;
+        //    ViewBag.Comment = comment;
+
+        //    System.IO.File.AppendAllText("comment.txt", $"{name}-{comment}\n");
+        //    return Redirect("CommentAdd");
+        //}
+        #endregion
     }
 }
